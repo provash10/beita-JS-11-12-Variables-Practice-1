0929dde68e4f464184e51c2dcd63fabaeab2878c
provash10 <provash20cb@gmail.com>
2024-07-28 09:41:55 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var pocket;
+pocket="70";
+console.log(pocket);
+
+// Again
+
+var pocket = "90";
+console.log(pocket);
+
+// Again
+
+var a = "17"
+var b = "19"
+var c = a+b;
+var d = a-b;
+var e = a*b;
+var f = a/b;
+var g = b%a;
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+
+// Again
+
+var firstName = "Goni";
+var lastName = "Mia";
+var age = 73;
+
+console.log(firstName,"", lastName,"is",age,"years old");
